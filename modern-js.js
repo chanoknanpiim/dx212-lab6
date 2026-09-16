faaa8376126c89fe8992a17e6654c6542c995861
Chanoknan Kositmongkolchai <chanoknan.piim@gmail.com>
2026-09-16 11:15:38 +0700

    Lab5

--- modern-js.js
+++ modern-js.js
@@ -8,3 +8,46 @@ const greet_modern = (name, faculty) => `สวัสดี ${name} จากค
 
 console.log(greet("Putter", " IT "));
 console.log(greet_modern("Putter", " IT "));
+
+/*ลองอ่านใหม่*/
+const student = {name:"ฟ้า", faculty:"CITU", year: 2};
+const updated = {...student, year: 3};
+
+console.log(student,updated);
+
+
+/*คือตัวแปรอันเลย์ เก็บตัวแปรอ็อบเจ็ก4ตัว*/
+const buses = [
+  { route: "NGV-1", passengers: 45, late: false },
+  { route: "NGV-2", passengers: 62, late: true },
+  { route: "NGV-3", passengers: 38, late: true },
+  { route: "NGV-4", passengers: 51, late: false },
+];
+ 
+for(let i = 0;i < buses.length;i++){
+    console.log(buses[i].route,buses[i].passengers,buses[i].late);
+}
+
+for (const bus of buses) {
+   // console.log(bus.route, bus.passengers, bus.late);
+}
+const routes = buses.map(bus => bus.route);
+console.log(routes);
+
+const lateBuses = buses.filter((bus) => bus.late === true);
+const heavyBuses = buses.filter((bus) => bus.passengers > 50);
+
+console.log(lateBuses);
+console.log(heavyBuses);
+
+const totalPassengers = buses.reduce(
+    (total,{passengers}) => total + passengers,0
+);      
+
+
+
+const totalPassengersHeavyBuses = buses.filter(({passengers}) => passengers > 50).reduce(
+    (total, {passengers}) => total + passengers, 0
+);
+console.log(totalPassengers);
+console.log(totalPassengersHeavyBuses);
